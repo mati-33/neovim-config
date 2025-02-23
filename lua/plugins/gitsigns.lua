c77ed86ac1d6e2192f0f9c5bc79200af764648b1
mati <mateusz.meksula@gmail.com>
2025-02-23 14:20:59 +0100

    initial config based on kickstart.nvim

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,38 @@
+-- Adds git related signs to the gutter, as well as utilities for managing changes
+return {
+	"lewis6991/gitsigns.nvim",
+	config = function()
+		require("gitsigns").setup({
+			signs = {
+				add = { text = "┃" },
+				change = { text = "┃" },
+				delete = { text = "" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+				untracked = { text = "┆" },
+			},
+			signs_staged = {
+				add = { text = "┃" },
+				change = { text = "┃" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+				untracked = { text = "┆" },
+			},
+			current_line_blame_opts = {
+				virt_text = true,
+				virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+				delay = 10,
+				ignore_whitespace = false,
+				virt_text_priority = 100,
+				use_focus = true,
+			},
+		})
+
+		vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk_inline<CR>", {})
+		vim.keymap.set("n", "<leader>bl", ":Gitsigns toggle_current_line_blame<CR>", {})
+		vim.keymap.set("n", "<leader>gb", ":Gitsigns blame_line<CR>", {})
+		vim.keymap.set({ "n", "v" }, "<leader>gs", ":Gitsigns stage_hunk<CR>", {})
+		vim.keymap.set({ "n", "v" }, "<leader>gS", ":Gitsigns undo_stage_hunk<CR>", {})
+	end,
+}
---
+++ lua/plugins/misc.lua
@@ -0,0 +1,29 @@
+-- Standalone plugins with less than 10 lines of config go here
+return {
+	{
+		-- Detect tabstop and shiftwidth automatically
+		"tpope/vim-sleuth",
+	},
+	{
+		-- Powerful Git integration for Vim
+		"tpope/vim-fugitive",
+	},
+	{
+		-- Hints keybinds
+		"folke/which-key.nvim",
+	},
+	{
+		-- Autoclose parentheses, brackets, quotes, etc.
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+		opts = {},
+	},
+	{
+		-- High-performance color highlighter
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup()
+		end,
+	},
+}

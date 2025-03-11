ccc17ab0f377b3f49e452de7fd63672c7dd48ba2
mati <mateusz.meksula@gmail.com>
2025-03-11 20:32:08 +0100

    multigit keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -73,9 +73,13 @@ return {
 		vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
 		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 		vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
-		vim.keymap.set("n", "<leader>mg", function()
-			require("telescope").extensions.multigit.multigit()
-		end, { desc = "[M]ulti[G]it", noremap = true })
+
+		vim.keymap.set("n", "<leader>mgf", function()
+			require("telescope").extensions.multigit.changed_files()
+		end, { desc = "[M]ulti[G]it changed [F]iles", noremap = true })
+		vim.keymap.set("n", "<leader>mgr", function()
+			require("telescope").extensions.multigit.repos()
+		end, { desc = "[M]ulti[G]it [R]epos", noremap = true })
 
 		-- Slightly advanced example of overriding default behavior and theme
 		vim.keymap.set("n", "<leader>/", function()

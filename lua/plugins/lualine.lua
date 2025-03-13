340df7483658b3737046ad6fbbb8924661b511be
mati <mateusz.meksula@gmail.com>
2025-03-13 20:30:15 +0100

    changes in lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,13 +1,6 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	config = function()
-		local mode = {
-			"mode",
-			fmt = function(str)
-				return " " .. str
-			end,
-		}
-
 		local hide_in_width = function()
 			return vim.fn.winwidth(0) > 100
 		end
@@ -30,13 +23,13 @@ return {
 				-- Some useful glyphs:
 				-- https://www.nerdfonts.com/cheat-sheet
 				--        
-				section_separators = { left = "", right = "" },
-				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+				component_separators = { left = "|", right = "|" },
 				disabled_filetypes = { "neo-tree" },
 				always_divide_middle = true,
 			},
 			sections = {
-				lualine_a = { mode },
+				lualine_a = { "mode" },
 				lualine_b = {},
 				lualine_c = { { "filename", path = 1 } },
 				lualine_x = {

fcabfde9a04bbdf4af3245aa7c078d648bebf477
mati <mateusz.meksula@gmail.com>
2025-04-25 18:17:35 +0200

    mini.files config

--- lua/plugins/mini_files.lua
+++ lua/plugins/mini_files.lua
@@ -1,14 +1,29 @@
 return {
 	"echasnovski/mini.files",
 	version = false,
-	config = true,
-	keys = {
-		{
-			"\\",
-			function()
-				require("mini.files").open()
-			end,
-			desc = "mini files",
-		},
-	},
+	lazy = false,
+	config = function()
+		local files = require("mini.files")
+		files.setup({
+			options = {
+				use_as_default_explorer = true,
+			},
+			mappings = {
+				synchronize = "s",
+			},
+		})
+
+		local minifiles_toggle = function()
+			if not files.close() then
+				files.open()
+			end
+		end
+
+		vim.keymap.set("n", "\\", function()
+			minifiles_toggle()
+		end, { desc = "toggle mini files" })
+		vim.keymap.set("n", "|", function()
+			files.open(vim.api.nvim_buf_get_name(0))
+		end, { desc = "open directory of current file" })
+	end,
 }
